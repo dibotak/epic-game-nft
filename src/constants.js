dcbdad84b0193e6ccd1c37cbd9addef795467f0b
Adib Faqih <foezst@gmail.com>
2021-11-12 22:18:17 +0700

    basic nft game

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xAAacadE964d46adC0DD332910fa648A38b6F0D4B';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber()
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
